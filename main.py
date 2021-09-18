c66e209487937986eeda2b0981db4f200f58483f
Phy Poh <phypoh@gmail.com>
2021-09-18 16:09:34 +0800

    Working repo added

---
+++ main.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#==============================================================================
+# Created on Tue Aug  1 21:00:09 2017
+#
+# @author: phypoh
+#==============================================================================
+
+import os
+import discord
+from discord.ext import commands
+
+
+bot = commands.Bot(command_prefix='!',
+                   owner_id=102704301956149248)
+
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError:
+    pass
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello, I'm Nephilim! My creator likes to call me Nephie.")
+
+@bot.command(name='reload', hidden=True)
+@commands.is_owner()
+async def _reload(ctx):
+    """Reloads the timeCog."""
+    try:
+        bot.unload_extension("timeCog")
+        bot.load_extension("timeCog")
+        await ctx.send("timeCog has been reloaded!")
+    except Exception as e:
+        await ctx.send('ERROR: {} - {}'.format(type(e).__name__, e))
+
+
+@bot.command(name='load', hidden=True)
+@commands.is_owner()
+async def _load(ctx):
+    """Reloads the timeCog."""
+    try:
+        bot.load_extension("timeCog")
+        await ctx.send("timeCog has been loaded!")
+    except Exception as e:
+        await ctx.send('ERROR: {} - {}'.format(type(e).__name__, e))
+
+@bot.command(name='unload', hidden=True)
+@commands.is_owner()
+async def _unload(ctx):
+    """Reloads the timeCog."""
+    try:
+        bot.unload_extension("timeCog")
+        await ctx.send("timeCog has been unloaded!")
+    except Exception as e:
+        await ctx.send('ERROR: {} - {}'.format(type(e).__name__, e))
+
+try:
+    bot.load_extension("timeCog")
+    print("timeCog has loaded!")
+except Exception as e:
+    print('ERROR: {} - {}'.format(type(e).__name__, e))
+
+bot.run(os.getenv('BOT_TOKEN'))
+
---
+++ timeCog.py
@@ -0,0 +1,76 @@
+import discord
+import asyncio
+import time
+from discord.ext import commands
+
+class timeCog(commands.Cog):
+    def __init__(self, _commands):
+        self.msglist = []
+        self.timelist = []
+
+    @commands.command()
+    async def test(self, ctx):
+        await ctx.send("Testing, testing. Cog is working!")
+
+    @commands.command()
+    async def remind(self, ctx, *, message:str):
+        'Set a reminder! Type !remind <X> <minutes/hours> <message> to remind yourself!'
+        try:
+            [num, period, msg] = message.split(' ', 2)
+            num = int(num)
+
+            if "hour" in period:
+                seconds = num*3600
+                self.timelist.append("{} hrs".format(num))
+            elif "min" in period:
+                seconds = num*60
+                self.timelist.append("{} mins".format(num))
+            elif "sec" in period:
+                seconds = num
+                self.timelist.append("{} secs".format(num))
+            else:
+                await ctx.send("Please send a valid period")
+
+            output = "Reminder has been set to recur every {num} {period} for the following message: \n{msg}".format(num=num, period=period, msg=msg)
+            await ctx.send(output)
+
+            self.msglist.append(msg)
+
+            await self.recurr(ctx, seconds, msg)
+
+        except Exception as e:
+            await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
+
+    async def recurr(self, ctx, seconds, msg):
+        if msg in self.msglist:
+            await asyncio.sleep(seconds)
+            embedVar = discord.Embed(title=msg, color=0x00ff00)
+            await ctx.send(embed=embedVar)
+            # await ctx.send(msg)
+            await self.recurr(ctx, seconds, msg)
+
+    @commands.command()
+    async def show(self, ctx):
+        if len(self.msglist) == 0:
+            await
+
+        output = "List of reminders:\n"
+        for i in range(len(self.msglist)):
+            output += "{}) ".format(i+1)
+            output += self.msglist[i]
+            output += " ("
+            output += self.timelist[i]
+            output += ")\n"
+        await ctx.send(output)
+
+    @commands.command()
+    async def remove(self, ctx, *, num:int):
+        output = "The following reminder has been removed: \n" + self.msglist[num+1]
+        del self.msglist[num+1]
+        del self.timelist[num + 1]
+        await ctx.send(output)
+
+
+
+def setup(bot):
+    bot.add_cog(timeCog(bot))
